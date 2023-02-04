1d95ee8152addc6206b6f9e6a969d5830146eeae
Rumube <rumube@floridauniversitaria.es>
2023-02-04 09:37:37 +0100

    Etiquetas Propias

--- Assets/Scripts/Perfil/PerfilPropio.cs
+++ Assets/Scripts/Perfil/PerfilPropio.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using TreeEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,7 +12,12 @@ public class PerfilPropio : Perfil
     public GameObject _nameTxt;
     public GameObject _bioTxt;
     public GameObject _edadTxt;
-    
+    public GameObject _etiquetaGO;
+    public GameObject _etiquetasParent;
+    [Header("Panel Etiquetas")]
+    public GameObject _panelEtiquetas;
+    public GameObject _panelEtiquetasParent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,14 +29,43 @@ public class PerfilPropio : Perfil
     {
         
     }
+    public void SetData()
+    {
+        if(_name != null)
+        {
+            _nameTxt.GetComponent<Text>().text = _name;
+            _bioTxt.GetComponent<Text>().text = _bio[0];
+            _edadTxt.GetComponent<Text>().text = _edad.ToString();
+            SetEtiquetas();
+        }
+    }
+    private void SetEtiquetas()
+    {
+        foreach (String currentEt in _etiquetas)
+        {
+            GameObject newEt = Instantiate(_etiquetaGO, _etiquetasParent.transform);
+            newEt.name = currentEt;
+            newEt.GetComponentInChildren<TextMeshProUGUI>().text = currentEt;
+        }
+    }
 
     #region boton
     public void SetPerfilData()
     {
         _name = _nameTxt.GetComponent<TMP_InputField>().text;
+        _bio.Clear();
         _bio.Add(_bioTxt.GetComponent<TMP_InputField>().text);
         _edad = int.Parse(_edadTxt.GetComponent<TMP_InputField>().text);
         GetComponent<GameManager>().CambiarEscena(GameManager.GAME_STATE.SELECCION);
+    }
+    public void InitPanelEtiquetas()
+    {
+        foreach (String currentEt in _posibleEtiquetas)
+        {
+            GameObject newEt = Instantiate(_etiquetaGO, _panelEtiquetasParent.transform);
+            newEt.name = currentEt;
+            newEt.GetComponentInChildren<TextMeshProUGUI>().text = currentEt;
+        }
     }
     #endregion
 }
