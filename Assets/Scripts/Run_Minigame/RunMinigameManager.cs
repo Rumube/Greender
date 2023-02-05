55f3240510b3c6d8a92f34ae03a9caf0005adf07
Rumube <rumube@floridauniversitaria.es>
2023-02-05 10:24:10 +0100

    Runner

--- Assets/Scripts/Run_Minigame/RunMinigameManager.cs
+++ Assets/Scripts/Run_Minigame/RunMinigameManager.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 public class RunMinigameManager : MonoBehaviour
 {
-
+    public GameObject _player;
     public GameObject _decorFondo;
 
     public GameObject _suelo;
@@ -13,29 +14,84 @@ public class RunMinigameManager : MonoBehaviour
 
     public float _speed;
 
-    public float _tiempo;
 
     public float _spawnTiempo;
+    public GameObject _spawnParent;
     public GameObject _obstaculo;
     public List<Sprite> _spritesFondo;
-    //private
+    public float _nextObs;
+
+    public bool _inGame = false;
+
+    public bool _inJump = false;
+    public GameObject _jumPos;
+    public GameObject _normalPos;
 
     private List<GameObject> _listObs = new List<GameObject>();
+    public float _finishGameTime;
+    public float _gameTime;
+    public TextMeshProUGUI _contador;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _nextObs = 0;
+        StartCoroutine(CuentaAtras());
     }
 
     // Update is called once per frame
     void Update()
     {
-        _suelo.transform.position = new Vector2(_speed, -703) * Time.deltaTime;
+        if (Time.realtimeSinceStartup >= _nextObs && _inGame)
+        {
+            _nextObs = Time.realtimeSinceStartup + _spawnTiempo;
+            SpawnObs();
+        }
+        foreach (GameObject currentObs in _listObs)
+        {
+            currentObs.transform.localPosition = new Vector2(currentObs.transform.localPosition.x - 1, 0);
+        }
     }
 
     private void SpawnObs()
     {
-        //new GameObject newObs = Instantiate(_obstaculo, _spawnTiempo);
+        GameObject newObs = Instantiate(_obstaculo, _spawnParent.transform);
+        _listObs.Add(newObs);
     }
+
+    public void Jump()
+    {
+        if(!_inJump)
+        {
+            _inJump = true;
+            _player.transform.position = _jumPos.transform.position;
+            StartCoroutine(FinSalto());
+        }
+
+    }
+
+    private IEnumerator FinSalto()
+    {
+        yield return new WaitForSeconds(1);
+        _inJump = false;
+        _player.transform.position = _normalPos.transform.position;
+    }
+
+    private IEnumerator CuentaAtras()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            _contador.text = (3 - i).ToString();
+            yield return new WaitForSeconds(1f);
+        }
+        StartGame();
+        yield return new WaitForSeconds(0.5f);
+        _contador.gameObject.SetActive(false);
+    }
+    private void StartGame()
+    {
+        _finishGameTime = Time.realtimeSinceStartup + _gameTime;
+        _inGame = true;
+    }
+
 }
